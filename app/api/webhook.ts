8e1a3c677e3795d018fb26d26d34f2ddffbd01d5
Tatchapoom <tatchapoom11@gmail.com>
2026-02-04 11:16:23 +0700

    update webhook

--- app/api/webhook.ts
+++ app/api/webhook.ts
@@ -4,11 +4,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-//     console.log("webhook request",req);
-//   if (req.method !== 'POST') {
-//     return res.status(405).end();
-//   }
-  const body = req.body;
-  console.log('Webhook body:', JSON.stringify(body, null, 2));
+  if (req.method !== 'POST') {
+    return res.status(200).end();
+  }
+  console.log('Webhook called');
+  console.log(JSON.stringify(req.body, null, 2));
+
   res.status(200).json({ success: true });
 }
