2d23ba279f8de12c11722a17a5407e9f075423e6
Tatchapoom <tatchapoom11@gmail.com>
2026-02-04 11:09:21 +0700

    comment check request mehtod

--- app/api/webhook.ts
+++ app/api/webhook.ts
@@ -4,10 +4,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    console.log("webhook request",req);
-  if (req.method !== 'POST') {
-    return res.status(405).end();
-  }
+//     console.log("webhook request",req);
+//   if (req.method !== 'POST') {
+//     return res.status(405).end();
+//   }
   const body = req.body;
   console.log('Webhook body:', JSON.stringify(body, null, 2));
   res.status(200).json({ success: true });
